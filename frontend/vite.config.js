07674cfb41ca3ed437c529921bd041860dd737ed
karnisinghji <your-karnisinghji-email@example.com>
2025-10-04 21:51:29 +0530

    remove JS config

---
+++ frontend/vite.config.js
@@ -0,0 +1,6 @@
+export default {
+  base: '/staff/',
+  build: {
+    outDir: 'dist'
+  }
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react'import react from '@vitejs/plugin-react';
+
+
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/staff/',
-  build: {
+  export default defineConfig({
+
+    plugins: [react()], plugins: [react()],
+
+    base: '/staff/'  base: '/staff/',
+
+  })  build: {
     outDir: 'dist',
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
